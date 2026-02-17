986b14a28642d050458483900fdca6adbc60ee8a
fredrik-cs <s.r.fredrik@student.vu.nl>
2026-02-17 16:12:35 +0100

    test fast response scanning protocol

--- src/scan.py
+++ src/scan.py
@@ -1,10 +1,15 @@
+from ast import Lambda
 from random import uniform
+import subprocess
 import time
 from pandas import read_excel
-from result import Ok, Err, Result
-from src.iw_interpret import Cell
-from src.ui import PrintCellSimple
-from src.enums import Data, WifiInterface
+from rusty_results import Ok, Err, Result # type: ignore
+from iw_interpret import Cell
+# from ui import PrintCellSimple
+from enums import Data, WifiInterface
+import pyperf
+import cProfile
+import re
 
 VU_IF = WifiInterface.WLP1S0
 AP_DF = read_excel(Data.AP_MAC)
@@ -61,7 +66,7 @@ def Scan(interface = VU_IF, frequencies = []) -> Result[list[Cell], str]:
     
     except Exception as e:
         #TODO: error handling
-        return Err(e)
+        return Err(str(e))
     
 def FilterAPs(cells) -> tuple[list[Cell], list[Cell]]:
     excel_aps = []
@@ -82,13 +87,16 @@ def FilterAPs(cells) -> tuple[list[Cell], list[Cell]]:
             other_aps.append(cell)
             
         # PrintCellSimple(cell, cell_index, in_excel)    
-        return excel_aps, other_aps
+    
+    return excel_aps, other_aps
         
 def FindAPs(ssid, frequencies, interface, min_aps = -1) -> tuple[list[Cell], list[Cell]]:
    
     cell_list = APsFromScanList(ssid, frequencies, interface)
     # cell_list = FindAtleastMinScans(ssid, interface, 3)
     # cell_list = Scan(interface)
+    excel_aps = []
+    other_aps = []
     
     match cell_list:
         
@@ -127,3 +135,94 @@ def FindAPs(ssid, frequencies, interface, min_aps = -1) -> tuple[list[Cell], lis
     print(excel_aps)
 
     return excel_aps, other_aps
+
+if __name__ == "__main__":
+    # frequency = int(input("Which frequency are we scanning?\n2412, 2437, 2462\n5180, 5200, 5220, 5240, 5260, 5280, 5300, 5320, 5500, 5520, 5560, 5580?\n"))
+    frequency = 5500
+    # runner = pyperf.Runner()
+    
+    def func():
+        min_ms_ago, max_ms_ago, all_results, good_amounts = ([],[],[],[])
+        sleep_time = 0.01
+        for i in range(1000):
+            # Cell.all(VU_IF, [frequency])
+            # try:
+            #     for j in range(10):
+            #         func1()
+            #         time.sleep(0.01)
+            # except: pass
+            func1()
+            min_now, max_now, results = func2()
+            min_ms_ago.append(min_now)
+            max_ms_ago.append(max_now)
+            all_results.append(results)
+            time.sleep(sleep_time)
+        min_ms_ago = sorted(min_ms_ago)
+        max_ms_ago = sorted(max_ms_ago, reverse=True)
+        print(f"Min: {min_ms_ago}")
+        print(f"Max: {max_ms_ago}")
+        for i, result in enumerate(all_results):
+            print(f"Result {i}: {result}")
+            good_result_amount = len(list(filter(lambda x: x>=-67, result)))
+            good_amounts.append(good_result_amount)
+        print(good_amounts)
+        print({x: good_amounts.count(x) for x in set(good_amounts)})
+
+
+    def func1():
+        # iwlist_scan = subprocess.check_output(
+        #             ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'freq', str(frequency), 'duration', '1000', 'flush'],
+        #                                                 stderr=subprocess.STDOUT)
+        # print(len(iwlist_scan))
+        # print(iwlist_scan)
+        # subprocess.run(
+        #     ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'duration', '1', 'freq', str(frequency), 'flush']
+        #     )
+        # print("func1 succeeded!")
+        # print(len(iwlist_scan))
+        # print(iwlist_scan)
+        # subprocess.run(
+        #     ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'freq', str(frequency), 'flush']
+        #     )
+        subprocess.run(
+            ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'flush']
+            )
+        # print("func1 succeeded!")
+    def func2():
+        try:
+            # iwlist_scan = subprocess.run(
+            #             ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'freq', str(frequency), 'duration', '1000', 'duration-mandatory', 'flush'],
+            #                                                 check=True, capture_output=True).stdout
+            iwlist_scan = subprocess.run(
+                ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'dump'],
+                        capture_output=True).stdout
+            # print(str(iwlist_scan))
+            pattern = r"(BSS (?:[\da-z]{2}:){5}[\da-z]{2})(?:.*?signal: )(-\d+\.\d* dBm)(?:\\n\\tlast seen: )(\d+ ms ago)(?:.*?)(SSID: [^\\]+)"
+            shrunk_scan = re.findall(pattern, str(iwlist_scan))
+            shrunk_scan = filter(lambda x: str(x[3]).endswith("VU-Campusnet"), shrunk_scan)
+            shrunk_scan = sorted(shrunk_scan, key=lambda x: int(x[2].split()[0]))
+            minimum = int(shrunk_scan[0][2].split()[0])
+            maximum = int(shrunk_scan[-1][2].split()[0])
+            OLDEST_IN_MS = 1000
+            shrunk_scan = list(filter(lambda x: int(x[2].split()[0]) <= OLDEST_IN_MS, shrunk_scan))
+            print(shrunk_scan)
+            results = map(lambda x: int(x[1].split(".")[0]), shrunk_scan)
+            
+            for result in shrunk_scan:
+                print(result)
+            print("func2 succeeded!")
+            return (minimum, maximum, sorted(list(results)))
+        except:
+            print("func2 failed!")
+            time.sleep(0.05)
+            return (9999, 0, 0)
+
+        #time.sleep(0.001)
+
+    # runner.bench_func('Cell.all', func)
+    cProfile.run('func()')  
+
+    # runner.timeit(name = "scan actively on one frequency",
+    #               stmt = "result = r",
+    #               setup = f"r = list(Cell.all({VU_IF}, {[freq]}))")
+    # Cell.all(VU_IF, [freq])
