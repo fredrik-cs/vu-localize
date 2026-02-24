bab81977725f88332b3c0804c2eef7eee05393b9
fredrik-cs <s.r.fredrik@student.vu.nl>
2026-02-24 13:31:35 +0100

    test code cleanup

--- src/scan.py
+++ src/scan.py
@@ -144,23 +144,21 @@ if __name__ == "__main__":
     def func():
         min_ms_ago, max_ms_ago, all_results, good_amounts = ([],[],[],[])
         sleep_time = 0.01
+
         for i in range(1000):
-            # Cell.all(VU_IF, [frequency])
-            # try:
-            #     for j in range(10):
-            #         func1()
-            #         time.sleep(0.01)
-            # except: pass
-            func1()
-            min_now, max_now, results = func2()
+            
+            trigger()
+            min_now, max_now, results = dump()
             min_ms_ago.append(min_now)
             max_ms_ago.append(max_now)
             all_results.append(results)
             time.sleep(sleep_time)
+        
         min_ms_ago = sorted(min_ms_ago)
         max_ms_ago = sorted(max_ms_ago, reverse=True)
         print(f"Min: {min_ms_ago}")
         print(f"Max: {max_ms_ago}")
+        
         for i, result in enumerate(all_results):
             print(f"Result {i}: {result}")
             good_result_amount = len(list(filter(lambda x: x>=-67, result)))
@@ -169,55 +167,45 @@ if __name__ == "__main__":
         print({x: good_amounts.count(x) for x in set(good_amounts)})
 
 
-    def func1():
-        # iwlist_scan = subprocess.check_output(
-        #             ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'freq', str(frequency), 'duration', '1000', 'flush'],
-        #                                                 stderr=subprocess.STDOUT)
-        # print(len(iwlist_scan))
-        # print(iwlist_scan)
-        # subprocess.run(
-        #     ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'duration', '1', 'freq', str(frequency), 'flush']
-        #     )
-        # print("func1 succeeded!")
-        # print(len(iwlist_scan))
-        # print(iwlist_scan)
-        # subprocess.run(
-        #     ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'freq', str(frequency), 'flush']
-        #     )
+    def trigger():
         subprocess.run(
             ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'trigger', 'flush']
             )
-        # print("func1 succeeded!")
-    def func2():
+        
+    def dump():
+        
         try:
-            # iwlist_scan = subprocess.run(
-            #             ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'freq', str(frequency), 'duration', '1000', 'duration-mandatory', 'flush'],
-            #                                                 check=True, capture_output=True).stdout
+            
             iwlist_scan = subprocess.run(
                 ['sudo', '/sbin/iw', 'dev', VU_IF, 'scan', 'dump'],
                         capture_output=True).stdout
             # print(str(iwlist_scan))
-            pattern = r"(BSS (?:[\da-z]{2}:){5}[\da-z]{2})(?:.*?signal: )(-\d+\.\d* dBm)(?:\\n\\tlast seen: )(\d+ ms ago)(?:.*?)(SSID: [^\\]+)"
-            shrunk_scan = re.findall(pattern, str(iwlist_scan))
-            shrunk_scan = filter(lambda x: str(x[3]).endswith("VU-Campusnet"), shrunk_scan)
-            shrunk_scan = sorted(shrunk_scan, key=lambda x: int(x[2].split()[0]))
-            minimum = int(shrunk_scan[0][2].split()[0])
-            maximum = int(shrunk_scan[-1][2].split()[0])
-            OLDEST_IN_MS = 1000
-            shrunk_scan = list(filter(lambda x: int(x[2].split()[0]) <= OLDEST_IN_MS, shrunk_scan))
-            print(shrunk_scan)
-            results = map(lambda x: int(x[1].split(".")[0]), shrunk_scan)
+
+            # 0 = BSS, 1 = frequency, 2 = signal, 3 = last seen, 4 = SSID
+            pattern = r"(?:BSS )((?:[\da-z]{2}:){5}[\da-z]{2})(?:.*?freq: )(\d*)(?:.*?signal: )(-\d+\.\d*)(?:.*?last seen: )(\d+)(?:.*?SSID: )([^\\]+)"
+            readable_scan = re.findall(pattern, str(iwlist_scan))
+            # print(readable_scan)
+            ssid_specific = filter(lambda scan_params: str(scan_params[4]).endswith("VU-Campusnet"), readable_scan)
+            chronological_scan = sorted(ssid_specific, key=lambda scan_params: int(scan_params[3]))
+            OLDEST_IN_MS = 6000
+            fresh_results = list(filter(lambda scan_params: int(scan_params[3].split()[0]) <= OLDEST_IN_MS, chronological_scan))
+
+            for result in fresh_results:
+                print(f"MAC <{result[0]}> (@freq: {result[1]} MHz) = {result[2]} dBm")
+                print(f"\tlast seen {result[3]} ms ago")
+
+            minimum = int(chronological_scan[0][2].split()[0])
+            maximum = int(chronological_scan[-1][2].split()[0])
+            signal_strengths = map(lambda scan_params: int(scan_params[2].split(".")[0]), fresh_results)
             
-            for result in shrunk_scan:
-                print(result)
-            print("func2 succeeded!")
-            return (minimum, maximum, sorted(list(results)))
+            print("scan dump succeeded!")
+            return (minimum, maximum, sorted(list(signal_strengths)))
+        
         except:
-            print("func2 failed!")
+            
+            print("scan dump failed!")
             time.sleep(0.05)
             return (9999, 0, 0)
-
-        #time.sleep(0.001)
 
     # runner.bench_func('Cell.all', func)
     cProfile.run('func()')  
